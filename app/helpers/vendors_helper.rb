a13ef68b0c63cdb3a8124501441aee8e314e3dd3
Cláudio Duarte <hello@claudioduarte.pt>
2024-11-21 22:13:20 +0000

    update countries endpoint

--- app/helpers/vendors_helper.rb
+++ app/helpers/vendors_helper.rb
@@ -2,16 +2,15 @@ module VendorsHelper
   def get_countries_helper
     @all_countries = []
 
-    countries = HTTParty.get('https://restcountries.eu/rest/v2/all')
+    countries = HTTParty.get('https://restcountries.com/v3.1/all')
     parsed_response = JSON.parse(countries.body)
 
     if countries.code == 200
       parsed_response.each do |c|
-        @all_countries << c["name"]
-        # puts @all_countries
+        @all_countries << c['name']['common']
       end
     else
-      puts "Unable to fetch countries"
+      flash[:error] = 'Unable to fetch countries'
     end
   end
 end
